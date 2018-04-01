f0cd6fdc6abdf3b295a23b28d1e5d339631d7c66
Craig <corsi007@umn.edu>
2018-03-31 22:55:09 -0500

    set up bootstrap page

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+currentSession = {
+    inGame = false,
+
+    characterAmethyst = {
+        name: 'Amethyst',
+        health: 140,
+        attack: 8,
+        counterAttack: 10
+    },
+
+    characterGarnet = {
+        name: 'Garnet',
+        health: 210,
+        attack: 4,
+        counterAttack: 20
+    },
+
+    characterPearl = {
+        name: 'Pearl',
+        health: 170,
+        attack: 6,
+        counterAttack: 15
+    },
+
+    characterSteven = {
+        name: 'Steven',
+        health: 90,
+        attack: 15,
+        counterAttack: 8
+    }
+}
