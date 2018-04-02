b19c3f6ca00914b6b2cc3a5c32ac6db1770c5cdc
Craig <corsi007@umn.edu>
2018-04-01 20:40:50 -0500

    initialize game object, working chooseCharacter method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,73 @@
-currentSession = {
-    inGame = false,
+currentGame = {
+    characterChosen: false,
+    inBattle: false,
 
-    characterAmethyst = {
+    characterAmethyst: {
         name: 'Amethyst',
         health: 140,
         attack: 8,
-        counterAttack: 10
+        counterAttack: 10,
+        playerChoice: false,
+        defeated: false
     },
 
-    characterGarnet = {
+    characterGarnet: {
         name: 'Garnet',
         health: 210,
         attack: 4,
-        counterAttack: 20
+        counterAttack: 20,
+        available: true
     },
 
-    characterPearl = {
+    characterPearl: {
         name: 'Pearl',
         health: 170,
         attack: 6,
-        counterAttack: 15
+        counterAttack: 15,
+        available: true
     },
 
-    characterSteven = {
+    characterSteven: {
         name: 'Steven',
         health: 90,
         attack: 15,
-        counterAttack: 8
+        counterAttack: 8,
+        available: true
+    },
+
+    remainingFighters: ['Amethyst', 'Garnet', 'Pearl', 'Steven'],
+
+    playerCharacter: null,
+    currentOpponent: null,
+
+    chooseCharacter: function(name) {
+        var myChar = $('#character' + name);
+        myChar.detach();
+        myChar.appendTo('#placeYourCharacterHere');
+    },
+
+    chooseOpponent: function(name) {
+
+    },
+
+    loseGame: function() {
+
+    },
+
+    winGame: function() {
+
+    },
+
+    defeatOpponent: function(opponentName) {
+
+    },
+
+    attackOpponent: function(yourAtk, theirHealth, theirAtk, yourHealth) {
+
     }
 }
+
+$('#characterAmethyst').on('click', function() {
+    currentGame.chooseCharacter('Amethyst');
+});
+
