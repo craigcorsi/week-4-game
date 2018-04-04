5722419a2827020ca1ee44f958b2ef00a4cac688
Craig <corsi007@umn.edu>
2018-04-03 20:56:58 -0500

    complete js game, resize images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,21 @@
+/*
+
+TO DO LIST:
+
+-Obtain square png files
+-Finish CSS style
+
+
+*/
+
 currentGame = {
-    characterChosen: false,
+    gameInProgress: true,
     inBattle: false,
+    currentHealth: '',
+    currentAttack: '',
+    playerBaseAttack: '',
+    currentOpponentHealth: '',
+    currentOpponentAttack: '',
 
     characterAmethyst: {
         name: 'Amethyst',
@@ -36,9 +51,6 @@ currentGame = {
 
     numberOfRemainingOpponents: 3,
 
-    playerCharacter: null,
-    currentOpponent: null,
-
     transferOpponent: function(name) {
         var myChar = $('#character' + name).parent();
         myChar.detach();
@@ -46,14 +58,27 @@ currentGame = {
     },
 
     chooseCharacter: function(name) {
+        // hide character selection menu
         $('#selectACharacter').hide();
+
+        // place chosen character in stats panel
         var myChar = $('#character' + name);
         myChar.detach();
         myChar.appendTo('#placeYourCharacterHere');
+
+        // set chosen character's health and attack power and update stats screen
+        this.currentHealth = this['character' + name]['health'];
+        this.currentAttack = this['character' + name]['attack'];
+        this.playerBaseAttack = this['character' + name]['attack'];
+        $('#currentHealth').html(this.currentHealth);
+        $('#currentAttack').html(this.currentAttack);
+
+        // place remaining characters into opponents menu
         $('#removeOpponentsFromHere .character').each(function(){
             currentGame.transferOpponent(this.name);
         });
-        currentGame.characterChosen = true;
+
+        // show opponent selection menu
         $('#selectAnOpponent').show();
     },
 
@@ -61,40 +86,82 @@ currentGame = {
         var myOpponent = $('#character' + name);
         myOpponent.parent().detach();
         myOpponent.appendTo('#currentOpponentGoesHere');
+        $('#buttonAttack').show();
 
+        // set chosen opponent's health and (counter) attack power and update stats screen
+        this.currentOpponentHealth = this['character' + name]['health'];
+        this.currentOpponentAttack = this['character' + name]['counterAttack'];
+        $('#currentOpponentHealth').html(this.currentOpponentHealth);
+        $('#currentOpponentAttack').html(this.currentOpponentAttack);
 
         // make the other opponents inaccessible until the battle is over
-        currentGame.inBattle = true;
+        this.inBattle = true;
         $('#placeOpponentsHere .character').parent().css({
             'opacity': '0.3'
         });
     },
 
     loseGame: function() {
-
+        this.gameInProgress = false;
+        $('#selectOppHeader').html('<p>YOU LOSE...</p>');
+        $('#battleGround').hide();
     },
 
     winGame: function() {
+        this.gameInProgress = false;
         $('#selectOppHeader').html('<p>YOU WIN!</p>');
+        $('#battleGround').hide();
     },
 
     defeatOpponent: function(opponentName) {
         $('#currentOpponentGoesHere').empty();
-        // make the other opponents inaccessible until the battle is over
-        currentGame.inBattle = false;
+
+        // Update stats, removing defeater opponents' stats
+        this.currentOpponentHealth = '';
+        this.currentOpponentAttack = '';
+        $('#currentOpponentHealth').html('');
+        $('#currentOpponentAttack').html('');
+
+        // Make other playable characters fightable
+        this.inBattle = false;
         $('#placeOpponentsHere .character').parent().css({
             'opacity': '1'
         });
 
-        currentGame.numberOfRemainingOpponents -= 1;
-        console.log(currentGame.numberOfRemainingOpponents);
+        // hide the attack button
+        $('#buttonAttack').hide();
 
-        if (currentGame.numberOfRemainingOpponents == 0) {
-            currentGame.winGame();
+        this.numberOfRemainingOpponents -= 1;
+        console.log(this.numberOfRemainingOpponents);
+
+        // Win the game if no remaining characters
+        if (this.numberOfRemainingOpponents == 0) {
+            this.winGame();
         }
     },
 
-    attackOpponent: function(yourAtk, theirHealth, theirAtk, yourHealth) {
+    attackOpponent: function(opponentName) {
+        // Player attacks. Reduce opponent's Health
+        this.currentOpponentHealth = Math.max(this.currentOpponentHealth - this.currentAttack, 0);
+        $('#currentOpponentHealth').html(this.currentOpponentHealth);
+
+        // Defeat opponent if opponent's health is 0
+        if (this.currentOpponentHealth == 0) {
+            this.defeatOpponent(opponentName);
+        }
+
+        // Increment your attack
+        this.currentAttack += this.playerBaseAttack;
+        $('#currentAttack').html(this.currentAttack);
+
+        // Opponent attacks. Reduce player's health
+        this.currentHealth = Math.max(this.currentHealth - this.currentOpponentAttack, 0);
+        $('#currentHealth').html(this.currentHealth);
+
+        // Lose game if player's health reaches 0.
+        if (this.gameInProgress && this.currentHealth == 0) {
+            this.loseGame();
+        }
 
     }
 }
@@ -103,19 +170,21 @@ $(document).ready(function() {
     /* Initial setting: hide the Opponent select screen */
     $('#selectAnOpponent').hide();
 
-
+    // Initial character selection
     $('#selectACharacter').on('click', '.character', function() {
         currentGame.chooseCharacter(this.name);
     });
 
+    // Opponent selection when not in battle
     $('#selectAnOpponent').on('click', '.character', function() {
         if (currentGame.inBattle == false) {
             currentGame.chooseOpponent(this.name);
         }
     });
 
+    // Attacking opponent
     $('#battleGround').on('click', '#buttonAttack', function() {
-        currentGame.defeatOpponent($('#battleGround .character').name);
+        currentGame.attackOpponent($('#battleGround .character').name);
     });
 
 });
